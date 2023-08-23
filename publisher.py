c07d0a197ca8d91227840f458a0c1680e9dca746
Caio Oliveira <73246374+caioOlivera@users.noreply.github.com>
2023-08-23 09:16:29 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,11 +1,41 @@
-import zmq, time
-from constPS import * #-
+import rpyc
 
-context = zmq.Context()
-s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp://"+ HOST +":"+ PORT      # how and where to communicate
-s.bind(p)                          # bind socket to the address
-while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+test = []
+myReferences = set()
+myNames = set()
+
+class MyService(rpyc.Service):
+    def on_connect(self, conn):
+        self.fn = None
+
+    def exposed_serverPrint(self, user, destination):
+        global myReferences
+        myReferences = {(fn, name) for fn, name in myReferences if name != user}
+        for i in myReferences:
+            if i[1] == destination and i[0] is not self.fn:
+                i[0](f"{user} joined the room")
+
+    def exposed_serverExit(self, name):
+        global myReferences
+        myReferences = {(fn, n) for fn, n in myReferences if n != name}
+
+    def exposed_serverPrintMessage(self, message, destination):
+        for i in myReferences:
+            if i[1] == destination and i[0] is not self.fn:
+                i[0](message)
+
+    def exposed_replyWith(self, number):
+        return test[number]
+
+    def exposed_replyLength(self, length):
+        return len(test)
+
+    def exposed_setCallback(self, fn, name):
+        self.fn = fn
+        myReferences.add((fn, name))
+        myNames.add(name)
+
+if __name__ == "__main__":
+    from rpyc.utils.server import ThreadedServer
+    t = ThreadedServer(MyService, port=18888)
+    t.start()
