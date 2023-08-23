2d712e243dc33423b547aacb341143782db976aa
Caio Oliveira <73246374+caioOlivera@users.noreply.github.com>
2023-08-23 09:16:57 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -1,12 +1,76 @@
+import rpyc
 import zmq
-from constPS import * #-
+from threading import Thread
 
-context = zmq.Context()
-s = context.socket(zmq.SUB)          # create a subscriber socket
-p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
-s.connect(p)                         # connect to the server
-s.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
+reach = 0
+net = None
+input_var = None
+user_name = None
 
-for i in range(5):  # Five iterations
-	time = s.recv()   # receive a message
-	print (bytes.decode(time))
+def myprint(message):
+    print(message)
+
+def checkAndPrint():
+    global reach, net
+    while True:
+        length = conn.root.replyLength(1)
+        if input_var == "exit":
+            break
+        while reach < length:
+            net = conn.root.replyWith(reach)
+            reach += 1
+            print(net)
+
+def receive_messages():
+    context = zmq.Context()
+    s_sub = context.socket(zmq.SUB)
+    s_sub.connect(f"tcp://{SERVER}:{PORT}")
+    s_sub.setsockopt_string(zmq.SUBSCRIBE, "GROUP")
+
+    while True:
+        message = s_sub.recv().decode()
+        if not message.startswith(f"GROUP {user_name}:"):
+            print(message)
+
+option = input("Do you want to send a direct message or a group message? (direct/group): ")
+
+if option == "direct":
+    conn = rpyc.connect(SERVER, PORT)
+    conn.root.setCallback(myprint, user_name)
+
+    user_name = input("Please enter your name: ")
+    destination = input("Please enter the name of the person you want to talk to: ")
+    print("Type exit to leave the conversation")
+    conn.root.serverPrint(user_name, destination)
+    reach = conn.root.replyLength(1)
+
+    t = Thread(target=checkAndPrint)
+    t.start()
+
+    while True:
+        input_var = input()
+        if input_var == "exit":
+            time.sleep(1)
+            input_var = f"{user_name} has left the conversation"
+            conn.root.setCallback(myprint, user_name)
+            conn.root.serverPrintMessage(input_var)
+            conn.root.serverExit(user_name)
+            break
+        reach += 1
+        input_var = f"{user_name}:{input_var}"
+        conn.root.setCallback(myprint, user_name)
+        conn.root.serverPrintMessage(input_var, destination)
+
+elif option == "group":
+    user_name = input("Please enter your name: ")
+
+    t = Thread(target=receive_messages)
+    t.start()
+
+    context = zmq.Context()
+    s = context.socket(zmq.PUB)
+    s.bind(f"tcp://{SERVER}:{PORT}")
+
+    while True:
+        msg = input("Enter a message: ")
+        s.send_string(f"GROUP {user_name}:{msg}")
